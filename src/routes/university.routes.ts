6801a70af50cb3df82f7e25f5f8728c999255793
antonio ricardo <antonio.gugou@gmail.com>
2023-03-20 10:47:31 -0300

    feat/adding-token-validation-in-create-update-and-delete-university-routes

--- src/routes/university.routes.ts
+++ src/routes/university.routes.ts
@@ -13,6 +13,7 @@ import {
 import Router from 'express'
 import universitiesControllers from '../controllers/university.controllers'
 import { validateRequest } from '../middlewares/validateRequest.middleware'
+import { validateToken } from '../middlewares/validateToken.middleware'
 
 const routes = Router()
 
@@ -24,35 +25,38 @@ routes.get(
 
 routes.get(
   '/',
-  async (req, res, next) =>
+  (req, res, next) =>
     validateRequest<ListUniversitiesDto>(req, res, next, listUniversitiesDto),
   async (req, res) => await universitiesControllers.listUniversities(req, res)
 )
 
 routes.get(
   '/:id',
-  async (req, res, next) =>
+  (req, res, next) =>
     validateRequest<GetUniversityByIdDto>(req, res, next, getUniversityByIdDto),
   async (req, res) => await universitiesControllers.getUniversityById(req, res)
 )
 
 routes.post(
   '/',
-  async (req, res, next) =>
+  (req, res, next) => validateToken(req, res, next),
+  (req, res, next) =>
     validateRequest<CreateUniversityDto>(req, res, next, createUniversityDto),
   async (req, res) => await universitiesControllers.createUniversity(req, res)
 )
 
 routes.put(
   '/:id',
-  async (req, res, next) =>
+  (req, res, next) => validateToken(req, res, next),
+  (req, res, next) =>
     validateRequest<UpdateUniversityDto>(req, res, next, updateUniversityDto),
   async (req, res) => await universitiesControllers.updateUniversity(req, res)
 )
 
 routes.delete(
   '/:id',
-  async (req, res, next) =>
+  (req, res, next) => validateToken(req, res, next),
+  (req, res, next) =>
     validateRequest<DeleteUniversityDto>(req, res, next, deleteUniversityDto),
   async (req, res) => await universitiesControllers.deleteUniversity(req, res)
 )
